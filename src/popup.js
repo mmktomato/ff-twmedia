01ab50ff3a98ab58d4b50564cfa092e39b00cac5
mmktomato <mkt.motohashi@gmail.com>
2019-08-18 00:37:04 +0900

    show video

--- src/popup.js
+++ src/popup.js
@@ -1,22 +1,40 @@
-import { getTweetId } from './util.js';
+import { getTweetId, getMediaArray, getVideoUrl } from './util.js';
 
 browser.tabs.query({ active: true, currentWindow: true }).then(tabs => {
+  const reload = document.querySelector('#reload');
+  reload.addEventListener('click', () => browser.tabs.reload(tabs[0].id));
+
   const tweetId = getTweetId(tabs[0].url);
   if (!tweetId) {
     return;
   }
 
   const listener = (details) => {
-    console.log(details.url);
+    // console.log(details.url);
     if (details.url.startsWith(`https://api.twitter.com/2/timeline/conversation/${tweetId}.json`)) {
       const decoder = new TextDecoder("utf-8");
       const filter = browser.webRequest.filterResponseData(details.requestId);
 
       filter.ondata = event => {
         const content = decoder.decode(event.data, { stream: true });
-        document.body.innerText = content;
+        const mediaArr = getMediaArray(JSON.parse(content), tweetId);
+        if (mediaArr) {
+          clearVideos();
+          hideReloadMessage();
 
-        // filter.write(event.data);
+          mediaArr.forEach(media => {
+            switch (media.type) {
+              case 'video':
+                const videoUrl = getVideoUrl(media);
+                if (videoUrl) {
+                  putVideo(videoUrl);
+                }
+                break;
+            }
+          });
+        }
+
+        filter.write(event.data);
         filter.disconnect();
       };
     }
@@ -28,3 +46,24 @@ browser.tabs.query({ active: true, currentWindow: true }).then(tabs => {
     ["blocking"]
   );
 });
+
+const hideReloadMessage = () => {
+  document.querySelector('#reload').style.display = 'none';
+};
+
+const clearVideos = () => {
+  const liArr = document.querySelectorAll('#videos > li');
+  if (liArr) {
+    liArr.forEach(li => li.remove());
+  }
+};
+
+const putVideo = (url) => {
+  const video = document.createElement('video');
+  video.src = url;
+  video.controls = true;
+  const li = document.createElement('li');
+  li.appendChild(video);
+  const ul = document.querySelector('#videos');
+  ul.appendChild(li);
+}
--- src/util.js
+++ src/util.js
@@ -5,3 +5,25 @@ export const getTweetId = (url) => {
 
   return m.length === 2 && m[1];
 };
+
+export const getMediaArray = (content, tweetId) => {
+  return content.globalObjects
+    && content.globalObjects.tweets
+    && content.globalObjects.tweets[tweetId]
+    && content.globalObjects.tweets[tweetId].extended_entities
+    && content.globalObjects.tweets[tweetId].extended_entities.media;
+};
+
+export const getVideoUrl = (media) => {
+  const variants = (media.video_info && media.video_info.variants) || [];
+  const largest = variants
+    .filter(v => v.content_type === 'video/mp4')
+    .reduce((acc, cur) => {
+      if (acc.bitrate < cur.bitrate) {
+        acc = cur;
+      }
+      return acc;
+    }, { bitrate: 0 });
+
+  return largest.url;
+};
